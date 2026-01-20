9dd4a4bb6e6368e99c0ad0fd79212ed18c74ae7d
yihan2588 <167504939+yihan2588@users.noreply.github.com>
2026-01-20 16:18:26 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/GRE-vocab/', // Add this line for GitHub Pages deployment
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/GRE-vocab/', // Add this line for GitHub Pages deployment
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    define: {
+      'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY || '')
+    }
+  };
 });
